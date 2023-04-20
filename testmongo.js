f019e77ea2d47cffbb67932a7867561bf41313a5
JayceHammond <hjayce6@gmail.com>
2023-04-20 12:45:36 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -166,10 +166,20 @@ app.patch("/rest/ticket/patch/:id", function (req, res) {
       const searchId = req.params.id;
       const query = { _id: parseInt(searchId) };
 
-      var updateTicket = {
-        body: req.body,
-      };
-      let result = await ticket.updateOne(query, updateTicket);
+      let result = await ticket.updateOne(query, {
+        createdAt: req.body.createdAt,
+        updatedAt: req.body.updatedAt,
+        type: req.body.type,
+        subject: req.body.subject,
+        Description: req.body.Description,
+        priority: req.body.priority,
+        status: req.body.status,
+        recipient: req.body.recipient,
+        submitter: req.body.submitter,
+        assignee_ID: req.body.assignee_ID,
+        follower_IDs: req.body.follower_IDs,
+        tags: req.body.tags,
+      });
       console.log(ticket);
       res.send(result).status(200);
     } finally {
