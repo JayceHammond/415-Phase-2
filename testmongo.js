a3f7fa78383d33b926513d6a67d06e396207f773
JayceHammond <hjayce6@gmail.com>
2023-04-20 12:25:40 -0500

    remove.push

--- testmongo.js
+++ testmongo.js
@@ -167,20 +167,18 @@ app.patch("/rest/ticket/patch/:id", function (req, res) {
       const query = { _id: parseInt(searchId) };
 
       var updateTicket = {
-        $push: {
-          createdAt: req.body.createdAt,
-          updatedAt: req.body.updatedAt,
-          type: req.body.type,
-          subject: req.body.subject,
-          Description: req.body.Description,
-          priority: req.body.priority,
-          status: req.body.status,
-          recipient: req.body.recipient,
-          submitter: req.body.submitter,
-          assignee_ID: req.body.assignee_ID,
-          follower_IDs: req.body.follower_IDs,
-          tags: req.body.tags,
-        },
+        createdAt: req.body.createdAt,
+        updatedAt: req.body.updatedAt,
+        type: req.body.type,
+        subject: req.body.subject,
+        Description: req.body.Description,
+        priority: req.body.priority,
+        status: req.body.status,
+        recipient: req.body.recipient,
+        submitter: req.body.submitter,
+        assignee_ID: req.body.assignee_ID,
+        follower_IDs: req.body.follower_IDs,
+        tags: req.body.tags,
       };
       let result = await ticket.updateOne(query, updateTicket);
       console.log(ticket);
